0db5ce4d307046243fc7fb7519ec0e406281aef4
akitohiko <raskisenpai@gmail.com>
2022-12-01 09:54:38 +0800

    update

--- components/Revenue.vue
+++ components/Revenue.vue
@@ -1,5 +1,5 @@
 <template>
-  <form action="https://hoshibox.deltaasterism.com" target="_blank" class="fixed md:static inset-x-0 bottom-0 bg-black sm:bg-transparent shadow sm:mt-5">
+  <form action="https://box.arkitosekai.net" target="_blank" class="fixed md:static inset-x-0 bottom-0 bg-black sm:bg-transparent shadow sm:mt-5">
     <button class="btn bg-white text-black hover:opacity-75 transition-color duration-200 uppercase px-3 py-2 font-bold ">✨看看专为你打造的星之箱</button>
   </form>
 </template>
